c0b63aac455481c18fc30aede94daba5e00a211e
Ana Hernandez-Smith <ana.hernandez7531@gmail.com>
2022-10-15 17:27:27 -0400

    Role entity added

---
+++ src/main/java/modele/Role.java
@@ -0,0 +1,39 @@
+package modele;
+
+public class Role {
+
+	private int idRole;
+	private String role;
+	
+	public Role(int idRole, String role) {
+		this.idRole = idRole;
+		this.role = role;
+		
+	}//Constructeur Role
+	
+	
+	//Getters & Setters
+	
+	public int getIdRole() {
+		return idRole;
+	}
+	
+	public void setIdRole(int idRole) {
+		this.idRole = idRole;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Role [idRole=" + idRole + ", role=" + role + "]";
+	}
+	
+	
+
+}
